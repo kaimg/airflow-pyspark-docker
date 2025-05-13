5b6a5884b8dc836b4637b0d08d51b9ca1f39d003
Kamran Gayibov <qayibovkamran@gmail.com>
2025-05-13 12:40:17 +0400

    Enhance currency rate ETL job by adding functionality to retrieve currency symbols and save them to a JSON file. Refactor API request handling and improve error management. Update output file paths for better organization.

--- scripts/currency_rate_etl_job.py
+++ scripts/currency_rate_etl_job.py
@@ -1,15 +1,33 @@
 import requests
 import polars as pl
 from config.config import API_KEY_CURRENCY_RATE
+from pathlib import Path
 
-API_URL = f"http://api.exchangeratesapi.io/v1/latest?access_key={API_KEY_CURRENCY_RATE}"
-def get_currency_rate(currency_code: str) -> float:
-    response = requests.get(f"{API_URL}&{currency_code}&symbols=USD,AUD,CAD,PLN,MXN")
+API_URL = f"http://api.exchangeratesapi.io/v1/"
+TARGET_CURRENCIES = ["USD","AUD","CAD","PLN","MXN"]
+
+OUTPUT_DIR = Path(__file__).parent / "data"
+OUTPUT_FILE = OUTPUT_DIR / "currency_rates.csv"
+OUTPUT_FILE_CURRENCIES = OUTPUT_DIR / "currencies_data.json"
+
+def get_currency_rate(currency_code: str="EUR") -> float:
+    symbols = ",".join(TARGET_CURRENCIES)
+    params = {
+        "access_key": API_KEY_CURRENCY_RATE,
+        "base": currency_code,
+        "symbols": symbols
+    }
+    response = requests.get(f"{API_URL}/latest", params=params)
+
+    if response.status_code != 200:
+        raise Exception("API request failed")
+    
     data = response.json()
-    print(data)
     if not data.get("success"):
-        raise Exception("API request failed or invalid response")
-
+        raise Exception(data.get("message", "Unknown error"))
+    
+    #print(data)
+    
     base = data["base"]
     date = data["date"]
     rates = data["rates"]
@@ -20,9 +38,20 @@ def get_currency_rate(currency_code: str) -> float:
     ]
 
     df = pl.DataFrame(rows)
-
+    df.write_csv(OUTPUT_FILE)
     return df
 
+def get_currencies_names():
+    params = {
+        "access_key": API_KEY_CURRENCY_RATE,
+    }
+    response = requests.get(f"{API_URL}/symbols", params=params)
+    data = response.json()
+
+    data = data["symbols"]
+    
+    df = pl.DataFrame(data)
+    df.write_json(OUTPUT_FILE_CURRENCIES)
+
 df = get_currency_rate("")
-df.write_csv("./data/currency_rates.csv")
 
