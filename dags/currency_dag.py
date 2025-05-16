7a1dd29bd7fc2e646fa9be602eebc4cb322f95fd
Kamran Gayibov <qayibovkamran@gmail.com>
2025-05-16 11:30:16 +0400

    Enhance currency rate ETL job by adding new dependencies for PyYAML and requests, updating the target currencies list, and refactoring the data handling to utilize PySpark for processing and loading currency rates into the database. Remove outdated currency rates CSV file.

---
+++ dags/currency_dag.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta
+from airflow import DAG  # type: ignore
+from airflow.operators.python import PythonOperator  # type: ignore
+from airflow.operators.empty import EmptyOperator  # type: ignore
+from scripts.currency_rate_etl_job import run_currency_etl_job
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id='currency_dag_python_operator',
+    default_args=default_args,
+    description='Run Currency ETL job using PythonOperator',
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    tags=['currency'],
+) as dag:
+
+    start = EmptyOperator(task_id='start')
+    end = EmptyOperator(task_id='end')
+
+    run_currency_job = PythonOperator(
+        task_id='run_currency_etl',
+        python_callable=run_currency_etl_job,
+        op_kwargs={},
+    )
+
+    (
+        start 
+        >> run_currency_job 
+        >> end
+    )
--- scripts/currency_rate_etl_job.py
+++ scripts/currency_rate_etl_job.py
@@ -1,20 +1,27 @@
 import requests
-import polars as pl # type: ignore
-from config.config import API_KEY_CURRENCY_RATE
 from pathlib import Path
+from config.config import API_KEY_CURRENCY_RATE, DB_CONFIG
+from pyspark.sql import SparkSession # type: ignore
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, DateType # type: ignore
+
 
 API_URL = f"http://api.exchangeratesapi.io/v1/"
-TARGET_CURRENCIES = ["USD","AUD","CAD","PLN","MXN"]
+TARGET_CURRENCIES = ["USD", "AUD", "CAD", "PLN", "MXN", "AZN", "TRY"]
 
-OUTPUT_DIR = Path(__file__).parent / "data"
-OUTPUT_FILE = OUTPUT_DIR / "currency_rates.csv"
-OUTPUT_FILE_CURRENCIES = OUTPUT_DIR / "currencies_data.json"
 
-def get_currency_rate(currency_code: str="EUR") -> float:
+def get_spark_session(app_name="Currency Rate ETL Job"):
+    spark = SparkSession.builder \
+        .appName(app_name) \
+        .config("spark.jars", DB_CONFIG["driver_path"]) \
+        .getOrCreate()
+    return spark
+
+
+def get_currency_rate(spark: SparkSession):
     symbols = ",".join(TARGET_CURRENCIES)
     params = {
         "access_key": API_KEY_CURRENCY_RATE,
-        "base": currency_code,
+        "base": "EUR",
         "symbols": symbols
     }
     response = requests.get(f"{API_URL}/latest", params=params)
@@ -26,32 +33,55 @@ def get_currency_rate(currency_code: str="EUR") -> float:
     if not data.get("success"):
         raise Exception(data.get("message", "Unknown error"))
     
-    #print(data)
-    
     base = data["base"]
     date = data["date"]
     rates = data["rates"]
 
     rows = [
-        {"Target Currency": currency, "Rate": rate, "Base": base, "Date": date}
+        {"target_currency": currency, "rate": rate, "base": base, "date": date}
         for currency, rate in rates.items()
     ]
 
-    df = pl.DataFrame(rows)
-    df.write_csv(OUTPUT_FILE)
+    # Define schema
+    schema = StructType([
+        StructField("target_currency", StringType(), True),
+        StructField("rate", DoubleType(), True),
+        StructField("base", StringType(), True),
+        StructField("date", StringType(), True)
+    ])
+
+    df = spark.createDataFrame(rows, schema=schema)
     return df
 
-def get_currencies_names():
-    params = {
-        "access_key": API_KEY_CURRENCY_RATE,
+
+def load_currency_rates_to_db(df, target_table="currency_rates", mode="overwrite"):
+    jdbc_url = DB_CONFIG["jdbc_url"]
+    properties = {
+        "user": DB_CONFIG["user"],
+        "password": DB_CONFIG["password"],
+        "driver": DB_CONFIG["driver"]
     }
-    response = requests.get(f"{API_URL}/symbols", params=params)
-    data = response.json()
 
-    data = data["symbols"]
-    
-    df = pl.DataFrame(data)
-    df.write_json(OUTPUT_FILE_CURRENCIES)
+    print(f"[Load] Writing {df.count()} records to {target_table}")
+    df.write.jdbc(
+        url=jdbc_url,
+        table=target_table,
+        mode=mode,
+        properties=properties
+    )
+    print("[Load] Write completed")
 
-df = get_currency_rate("")
 
+def run_currency_etl_job():
+    spark = get_spark_session()
+    try:
+        df = get_currency_rate(spark)
+        load_currency_rates_to_db(df, mode="overwrite")
+        df.show(5)
+    finally:
+        spark.stop()
+        print("[Shutdown] Spark session stopped")
+
+
+if __name__ == "__main__":
+    run_currency_etl_job()
